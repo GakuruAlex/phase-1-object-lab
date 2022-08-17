9abd5ad2fe24322c866cc28ebad691ad0f4b60da
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-08-17 21:40:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution in this file!
+
+const employee ={
+    name :`Alex`,
+    addrees :`20109-Nakuru`,
+}
+
+const updateEmployeeWithKeyAndValue =(employee,key,value)=>{
+
+const newEmployee ={...employee};
+
+newEmployee[key]=value;
+
+return newEmployee;
+
+}
+
+const destructivelyDeleteFromEmployeeByKey =(employee,key)=>{
+
+delete employee[key];
+
+return employee;
+}
+
+//updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee:
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value)=>{
+
+employee[key]=value;
+
+return employee;
+
+}
+
+//deletes `key` from a clone of employee and returns the new employee (it is non-destructive):
+const deleteFromEmployeeByKey = (employee, key)=>{
+
+const newEmployee={...employee};
+
+delete newEmployee[key];
+
+return newEmployee;
+
+}
+
+
+
+console.log(updateEmployeeWithKeyAndValue(employee,`language`,`JavaScript`));
